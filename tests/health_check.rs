35b65618a4c9e27861512fbc5d1948e06a9fe4f1
Timon Viola <44016238+timonviola@users.noreply.github.com>
2024-05-27 08:01:11 +0200

    3.10

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,15 +1,31 @@
-use sqlx::{Connection, PgConnection};
+use sqlx::{Connection, PgConnection, PgPool};
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
-fn spawn_app() -> String {
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
+
+
+async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
 
     let port = listener.local_addr().unwrap().port();
-    let server = run(listener).expect("Failed to bind address.");
+    let address = format!("http://127.0.0.1:{}", port);
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
+
+    let server = run(listener, connection_pool.clone()).expect("Failed to bind address.");
     let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
+    TestApp {
+        address,
+        db_pool: connection_pool
+    }
 }
 
 #[tokio::test]
