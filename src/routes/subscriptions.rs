1c94f861d883228f277c5963610075b3b1f26313
Timon Viola <44016238+timonviola@users.noreply.github.com>
2024-06-11 19:46:24 +0200

    4.5.8

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -15,13 +15,13 @@ pub async fn subscribe(
     connection_pool: web::Data<PgPool>,
 ) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    log::info!(
+    tracing::info!(
         "request_id {} - Adding '{}' '{}' as a new subscriber.",
         request_id,
         form.email,
         form.name
     );
-    log::info!(
+    tracing::info!(
         "request_id {} - Saving new subscriber details in the database",
         request_id
     );
@@ -39,14 +39,14 @@ pub async fn subscribe(
     .await
     {
         Ok(_) => {
-            log::info!(
+            tracing::info!(
                 "request_id {} - New subscriber details have been saved.",
                 request_id
             );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!(
+            tracing::error!(
                 "request_id {} -Failed to execute query: {:?}",
                 request_id,
                 e
