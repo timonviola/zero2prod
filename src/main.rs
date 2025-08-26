78ef9dec34ff07b16fd5167aca9cc19b6a6d5692
Timon Viola <44016238+timonviola@users.noreply.github.com>
2025-08-26 21:11:10 +0200

    5.3.8

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use secrecy::ExposeSecret;
@@ -11,7 +12,9 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
         .expect("Failed to connect to Postgres.");
     let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address)?;
