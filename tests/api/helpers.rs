eda63274df7d600d89eff19247fc61a2c3338b4c
Timon Viola <44016238+timonviola@users.noreply.github.com>
2025-09-21 11:44:29 +0200

    7.3.8

---
+++ tests/api/helpers.rs
@@ -0,0 +1,80 @@
+use sqlx::{Connection, Executor, PgConnection, PgPool};
+use actix_web::dev::Server;
+use tokio::net::TcpListener;
+use uuid::Uuid;
+use once_cell::sync::Lazy;
+use std::io::{sink, stdout};
+
+
+
+use zero2prod::configuration::{self, get_configuration, DatabaseSettings, Settings};
+use zero2prod::email_client::EmailClient;
+use zero2prod::startup::{Application, get_connection_pool};
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber("test".into(), "debug".into(), stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber("test".into(), "debug".into(), sink);
+        init_subscriber(subscriber);
+    };
+});
+
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
+
+pub async fn spawn_app() -> TestApp {
+    // The first time `initialize` is invoked the code in `TRACING` is executed.
+    // All other invocations will instead skip execution.
+    Lazy::force(&TRACING);
+
+    let configuration = {
+        let mut c = get_configuration().expect("Failed to read configuration.");
+        c.database.database_name =  Uuid::new_v4().to_string();
+        c.application.port = 0;
+        c
+    };
+
+    let application = Application::build(configuration.clone())
+        .await
+        .expect("Failed to build application.");
+
+    let address = format!("http://127.0.0.1:{}", application.port());
+    let _ = tokio::spawn(application.run_until_stopped());
+    TestApp {
+        address,
+        db_pool: get_connection_pool(&configuration.database),
+    }
+}
+
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    let mut connection = PgConnection::connect_with(&config.without_db())
+        .await
+        .expect("Failed to cennect to Postgres");
+
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database.");
+
+    // Migrate database
+    let connection_pool = PgPool::connect_with(config.with_db())
+        .await
+        .expect("Failed to connect to Postgres");
+
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database.");
+
+    connection_pool
+}
+
--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -1,102 +1,5 @@
-use once_cell::sync::Lazy;
-use secrecy::ExposeSecret;
-use sqlx::{Connection, Executor, PgConnection, PgPool};
-use std::io::{sink, stdout};
-use std::net::TcpListener;
-use uuid::Uuid;
-use zero2prod::email_client::{self, EmailClient};
+use crate::helpers::spawn_app;
 
-use zero2prod::configuration::{get_configuration, DatabaseSettings};
-use zero2prod::startup::run;
-use zero2prod::telemetry::{get_subscriber, init_subscriber};
-
-static TRACING: Lazy<()> = Lazy::new(|| {
-    let default_filter_level = "info".to_string();
-    let subscriber_name = "test".to_string();
-    if std::env::var("TEST_LOG").is_ok() {
-        let subscriber = get_subscriber("test".into(), "debug".into(), stdout);
-        init_subscriber(subscriber);
-    } else {
-        let subscriber = get_subscriber("test".into(), "debug".into(), sink);
-        init_subscriber(subscriber);
-    };
-});
-
-pub struct TestApp {
-    pub address: String,
-    pub db_pool: PgPool,
-}
-
-async fn spawn_app() -> TestApp {
-    // The first time `initialize` is invoked the code in `TRACING` is executed.
-    // All other invocations will instead skip execution.
-    Lazy::force(&TRACING);
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
-    let port = listener.local_addr().unwrap().port();
-    let address = format!("http://127.0.0.1:{}", port);
-
-    let mut configuration = get_configuration().expect("Failed to read configuration.");
-    configuration.database.database_name = Uuid::new_v4().to_string();
-    let connection_pool = configure_database(&configuration.database).await;
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    let server =
-        run(listener, connection_pool.clone(), email_client).expect("Failed to bind address.");
-    let _ = tokio::spawn(server);
-    TestApp {
-        address,
-        db_pool: connection_pool,
-    }
-}
-
-async fn configure_database(config: &DatabaseSettings) -> PgPool {
-    let mut connection = PgConnection::connect_with(&config.without_db())
-        .await
-        .expect("Failed to cennect to Postgres");
-
-    connection
-        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
-        .await
-        .expect("Failed to create database.");
-
-    // Migrate database
-    let connection_pool = PgPool::connect_with(config.with_db())
-        .await
-        .expect("Failed to connect to Postgres");
-
-    sqlx::migrate!("./migrations")
-        .run(&connection_pool)
-        .await
-        .expect("Failed to migrate the database.");
-
-    connection_pool
-}
-
-#[tokio::test]
-async fn health_check_works() {
-    let test_app = spawn_app().await;
-    let client = reqwest::Client::new();
-
-    //Act
-    let response = client
-        .get(&format!("{}/health_check", &test_app.address))
-        .send()
-        .await
-        .expect("Failed to execute request.");
-
-    assert!(response.status().is_success());
-    assert_eq!(Some(0), response.content_length());
-}
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
@@ -206,3 +109,4 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
         );
     }
 }
+
