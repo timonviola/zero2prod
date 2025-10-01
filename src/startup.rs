07c886a2fad11060e6d7f7d43a2de2240492b70a
Timon Viola <44016238+timonviola@users.noreply.github.com>
2025-10-01 20:54:28 +0200

    7.7.5

--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,7 @@ use crate::configuration::Settings;
 use crate::configuration::DatabaseSettings;
 use crate::email_client::EmailClient;
 use crate::routes::{health_check, subscribe};
+use crate::routes::confirm;
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
@@ -24,6 +25,7 @@ pub fn run(
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .route("/subscriptions/confirm", web::get().to(confirm))
             .app_data(connection_pool.clone())
             .app_data(email_client.clone())
     })
