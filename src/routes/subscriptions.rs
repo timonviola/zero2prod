370b6697932f2e27eabbae89ce556bce73100a13
Timon Viola <44016238+timonviola@users.noreply.github.com>
2025-10-01 22:17:27 +0200

    format code

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -65,7 +65,10 @@ pub async fn send_confirmation_email(
     new_subscriber: NewSubscriber,
     base_url: &str,
 ) -> Result<(), reqwest::Error> {
-    let confirmation_link = format!("{}/subscriptions/confirm?subscription_token=mytoken", base_url);
+    let confirmation_link = format!(
+        "{}/subscriptions/confirm?subscription_token=mytoken",
+        base_url
+    );
     email_client
         .send_email(
             new_subscriber.email,
