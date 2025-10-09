a494b659242e5c1f5fadbe2f1935f52888003e9a
Timon Viola <44016238+timonviola@users.noreply.github.com>
2025-10-09 19:10:06 +0200

    8.3

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -35,7 +35,6 @@ fn generate_subscription_token() -> String {
         .collect()
 }
 
-#[derive(Debug)]
 pub struct StoreTokenError(sqlx::Error);
 
 impl std::fmt::Display for StoreTokenError {
@@ -48,7 +47,32 @@ impl std::fmt::Display for StoreTokenError {
     }
 }
 
+fn error_chain_fmt(
+    e: &impl std::error::Error,
+    f: &mut std::fmt::Formatter<'_>,
+) -> std::fmt::Result {
+    writeln!(f, "{}\n", e)?;
+    let mut current = e.source();
+    while let Some(cause) = current {
+        writeln!(f, "Caused by:\n\t{}", cause)?;
+        current = cause.source();
+    }
+    Ok(())
+}
+
+impl std::fmt::Debug for StoreTokenError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
 impl ResponseError for StoreTokenError {}
+
+impl std::error::Error for StoreTokenError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        Some(&self.0)
+    }
+}
 
 #[tracing::instrument(
   name = "Adding a new subscriber",
