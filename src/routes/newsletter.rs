518843b01bfe91d4e331fcf463b916aafb778a32
Timon Viola <44016238+timonviola@users.noreply.github.com>
2025-10-19 12:25:34 +0200

    9.3

---
+++ src/routes/newsletter.rs
@@ -0,0 +1,7 @@
+use actix_web::HttpResponse;
+
+
+pub async fn publish_newsletter() -> HttpResponse {
+
+    HttpResponse::Ok().finish()
+}
--- src/startup.rs
+++ src/startup.rs
@@ -1,8 +1,7 @@
 use crate::configuration::DatabaseSettings;
 use crate::configuration::Settings;
 use crate::email_client::EmailClient;
-use crate::routes::confirm;
-use crate::routes::{health_check, subscribe};
+use crate::routes::{confirm, health_check,publish_newsletter, subscribe};
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
@@ -81,6 +80,7 @@ pub fn run(
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
+            .route("/newsletters", web::post().to(publish_newsletter))
             .app_data(connection_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
