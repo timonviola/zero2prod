dcdbb265e704bd0f7b6455f1faa7dfb92624c0e8
Timon Viola <44016238+timonviola@users.noreply.github.com>
2025-10-19 13:29:57 +0200

    9.6

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -25,16 +25,10 @@ async fn newsletters_are_delivered_to_confirmed_subscribers() {
       }
     });
 
-    let response = reqwest::Client::new()
-        .post(&format!("{}/newsletters", &app.address))
-        .json(&newsletter_request_body)
-        .send()
-        .await
-        .expect("Failed to execute request.");
-
+    let response = app.post_newsletters(newsletter_request_body).await;
     assert_eq!(response.status().as_u16(), 200);
-    
 }
+
 #[tokio::test]
 async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
     let app = spawn_app().await;
@@ -57,15 +51,41 @@ async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
       }
     });
 
-    let response = reqwest::Client::new()
-        .post(&format!("{}/newsletters", &app.address))
-        .json(&newsletter_request_body)
-        .send()
-        .await
-        .expect("Failed to execute request.");
-
+    let response = app.post_newsletters(newsletter_request_body).await;
     assert_eq!(response.status().as_u16(), 200);
     
+}
+
+#[tokio::test]
+async fn newsletters_return_400_for_invalid_data() {
+    let app = spawn_app().await;
+
+    let test_cases = vec![
+        (
+            serde_json::json!({
+                "content": {
+                    "text": "Newsletter body as plain text",
+                    "html": "<p>Newsletter body as HTML</p>",
+                }
+            }),
+            "missing title",
+        ),
+        (
+            serde_json::json!({"title": "Newsletter!"}),
+            "missing content",
+        ),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = app.post_newsletters(invalid_body).await;
+    
+
+        assert_eq!(400, 
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 }
 
 async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
