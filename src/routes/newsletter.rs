1bf13ee980d2678a58506eb015f32da20d2649df
Timon Viola <44016238+timonviola@users.noreply.github.com>
2025-10-25 20:15:04 +0200

    10.2.5

--- src/routes/newsletter.rs
+++ src/routes/newsletter.rs
@@ -1,6 +1,7 @@
 use crate::domain::SubscriberEmail;
 use crate::email_client::EmailClient;
 use crate::routes::error_chain_fmt;
+use crate::telemetry::spawn_blocking_with_tracing;
 use actix_web::http::header::{HeaderMap, HeaderValue};
 use actix_web::http::{header, StatusCode};
 use actix_web::web;
@@ -8,7 +9,9 @@ use actix_web::HttpRequest;
 use actix_web::HttpResponse;
 use actix_web::ResponseError;
 use anyhow::Context;
-use argon2::{Algorithm, Argon2, Params, PasswordHash, PasswordHasher, PasswordVerifier, Version, Error};
+use argon2::{
+    Algorithm, Argon2, Error, Params, PasswordHash, PasswordHasher, PasswordVerifier, Version,
+};
 use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
 
@@ -126,24 +129,35 @@ async fn validate_credentials(
         .map_err(PublishError::UnexpectedError)?
         .ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username.")))?;
 
-    let expected_password_hash = PasswordHash::new(expected_password_hash.expose_secret())
-        .context("Failed to parse hash in PHC string format.")?;
-    //let expected_password_hash = PasswordHash::new(
-    //    &expected_password_hash.expose_secret()
-    //)
-    //.map_err(PublishError::UnexpectedError)?;
 
-    tracing::info_span!("Verify password hash")
-        .in_scope(|| {
-            Argon2::default().verify_password(
-                credentials.password.expose_secret().as_bytes(),
-                &expected_password_hash,
-            )
-        })
-        .context("Invalid password.")
-        .map_err(PublishError::AuthError)?;
+    spawn_blocking_with_tracing(move || {
+          verify_password_hash(expected_password_hash, credentials.password)
+    })
+    .await
+    .context("Failed to spawn blocking task.")
+    .map_err(PublishError::UnexpectedError)?;
 
     Ok(user_id)
+}
+
+#[tracing::instrument(name = "Verify password hash", skip(expected_password_hash, password_candidate))]
+fn verify_password_hash(
+    expected_password_hash: Secret<String>,
+    password_candidate: Secret<String>,
+) -> Result<(), PublishError> {
+
+    let expected_password_hash = PasswordHash::new(
+        expected_password_hash.expose_secret()
+    )
+    .context("Failed to parse hash in PHC string format.")
+    .map_err(PublishError::UnexpectedError)?;
+
+    Argon2::default().verify_password(
+        password_candidate.expose_secret().as_bytes(),
+        &expected_password_hash,
+    )
+    .context("Invalid password.")
+    .map_err(PublishError::AuthError)
 }
 
 #[tracing::instrument(name = "Get stored credentials", skip(username, pool))]
