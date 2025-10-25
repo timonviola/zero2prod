129523ffe4e9f5e6dc60f556412fa6cf9544ad5a
Timon Viola <44016238+timonviola@users.noreply.github.com>
2025-10-25 21:23:54 +0200

    10.2.5

--- src/routes/newsletter.rs
+++ src/routes/newsletter.rs
@@ -129,35 +129,35 @@ async fn validate_credentials(
         .map_err(PublishError::UnexpectedError)?
         .ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username.")))?;
 
-
     spawn_blocking_with_tracing(move || {
-          verify_password_hash(expected_password_hash, credentials.password)
+        verify_password_hash(expected_password_hash, credentials.password)
     })
     .await
     .context("Failed to spawn blocking task.")
-    .map_err(PublishError::UnexpectedError)?;
+    .map_err(PublishError::UnexpectedError)??;
 
     Ok(user_id)
 }
 
-#[tracing::instrument(name = "Verify password hash", skip(expected_password_hash, password_candidate))]
+#[tracing::instrument(
+    name = "Verify password hash",
+    skip(expected_password_hash, password_candidate)
+)]
 fn verify_password_hash(
     expected_password_hash: Secret<String>,
     password_candidate: Secret<String>,
 ) -> Result<(), PublishError> {
+    let expected_password_hash = PasswordHash::new(expected_password_hash.expose_secret())
+        .context("Failed to parse hash in PHC string format.")
+        .map_err(PublishError::UnexpectedError)?;
 
-    let expected_password_hash = PasswordHash::new(
-        expected_password_hash.expose_secret()
-    )
-    .context("Failed to parse hash in PHC string format.")
-    .map_err(PublishError::UnexpectedError)?;
-
-    Argon2::default().verify_password(
-        password_candidate.expose_secret().as_bytes(),
-        &expected_password_hash,
-    )
-    .context("Invalid password.")
-    .map_err(PublishError::AuthError)
+    Argon2::default()
+        .verify_password(
+            password_candidate.expose_secret().as_bytes(),
+            &expected_password_hash,
+        )
+        .context("Invalid password.")
+        .map_err(PublishError::AuthError)
 }
 
 #[tracing::instrument(name = "Get stored credentials", skip(username, pool))]
--- src/telemetry.rs
+++ src/telemetry.rs
@@ -29,7 +29,7 @@ pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
 }
 
 pub fn spawn_blocking_with_tracing<F, R>(f: F) -> JoinHandle<R>
-where 
+where
     F: FnOnce() -> R + Send + 'static,
     R: Send + 'static,
 {
