12bc906f8f3c4a0124920d418520e1a0467ddfcc
Timon Viola <44016238+timonviola@users.noreply.github.com>
2025-10-26 11:31:03 +0100

    10.6.3.2

--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,9 @@
 use crate::configuration::DatabaseSettings;
 use crate::configuration::Settings;
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, publish_newsletter, subscribe, home};
+use crate::routes::{
+    confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
+};
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
@@ -80,6 +82,8 @@ pub fn run(
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
             .route("/newsletters", web::post().to(publish_newsletter))
+            .route("/login", web::get().to(login_form))
+            .route("/login", web::post().to(login))
             .route("/", web::get().to(home))
             .app_data(connection_pool.clone())
             .app_data(email_client.clone())
