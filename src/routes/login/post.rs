e6e83e727b58a96c784c7204da591b14708ba802
Timon Viola <44016238+timonviola@users.noreply.github.com>
2025-10-29 17:43:39 +0100

    10.7.5.4

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,6 +1,8 @@
 use crate::authentication::AuthError;
 use crate::authentication::{validate_credentials, Credentials};
 use crate::routes::error_chain_fmt;
+use crate::session_state::TypedSession;
+
 use actix_web::error::InternalError;
 use actix_web::http::header::LOCATION;
 use actix_web::http::StatusCode;
@@ -12,6 +14,7 @@ use actix_web_flash_messages::FlashMessage;
 use secrecy::Secret;
 use sqlx::PgPool;
 use actix_session::Session;
+
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -26,7 +29,7 @@ pub struct FormData {
 pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
-    session: Session,
+    session: TypedSession,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.0.username,
@@ -38,7 +41,8 @@ pub async fn login(
     match validate_credentials(credentials, &pool).await {
         Ok(user_id) => {
             tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
-            session.insert("user_id", user_id)
+            session.renew();
+            session.insert_user_id(user_id)
                 .map_err(|e| login_redirect(LoginError::UnexpectedError(e.into())))?;
             Ok(HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/admin/dashboard"))
